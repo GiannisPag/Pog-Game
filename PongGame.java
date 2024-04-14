2cb25bfc7c5fd7e8af151eff8d122325f33153df
GiannisPag <153856039+GiannisPag@users.noreply.github.com>
2024-04-14 11:07:01 +0300

    Add files via upload

---
+++ PongGame.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PongGame {
+
+    public static void main(String[] args) {
+
+        GameProg prog = new GameProg();
+        
+    }
+}
